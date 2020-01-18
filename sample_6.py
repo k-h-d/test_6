de578bfbaeab1edd8d479d682cb2c59aaea7a0fa
平塚圭太 <g.h.py.2456@gmail.com>
2020-01-18 16:14:48 +0900

    かずあて

---
+++ sample_6.py
@@ -0,0 +1,15 @@
+import random as rd
+cnt=0
+
+while cnt<3:
+    j=input("1～10の数字入力：")
+    if j.isdigit():
+        a=rd.randint(1,4)
+        if int(j)==a:
+            print("正解")
+        else:
+            print("不正解")
+    else:
+        print("数字のみ入力")
+        continue
+    cnt+=1
