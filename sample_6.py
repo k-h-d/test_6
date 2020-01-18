c23e1f9c452a2bea8d43c7ba74423654bf1817c5
平塚圭太 <g.h.py.2456@gmail.com>
2020-01-18 16:18:03 +0900

    変更

--- sample_6.py
+++ sample_6.py
@@ -4,7 +4,7 @@ cnt=0
 while cnt<3:
     j=input("1～10の数字入力：")
     if j.isdigit():
-        a=rd.randint(1,4)
+        a=rd.randint(1,11)
         if int(j)==a:
             print("正解")
         else:
